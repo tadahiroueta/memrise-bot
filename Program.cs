cf8e82dc026b87e5210167d13eea4218cfa6e647
tadahiroueta <tadahiroueta@gmail.com>
2024-08-20 12:39:08 -0500

    working on different questions

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿using MemriseBot.src;
+﻿using MemriseBot;
 
 class Program {
     static void Main() {
+        // testing Crawler
         Crawler crawler = new Crawler();
         crawler.Login();
         crawler.Play();
+
+        // testing Translator
+        // Translator translator = new Translator();
+        // translator.learn("hola", "hello");
+        // translator.learn("adios", "goodbye");
+        // translator.learn("gracias", "thank you");
+        // translator.learn("por favor", "please");
+        // // print
+        // Console.WriteLine(translator.translate("hola"));
+        // Console.WriteLine(translator.translateBack("hello"));
+        // Console.WriteLine(translator.translate("adios"));
+        // Console.WriteLine(translator.translateBack("goodbye"));
     }
 }
--- src/Crawler.cs
+++ src/Crawler.cs
@@ -1,19 +1,21 @@
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace MemriseBot.src {
+namespace MemriseBot {
     /// <summary>
     /// Responsible for crawling the Memrise website.
     /// </summary>
     public class Crawler {
 
-        private const string CookiesPath = "../../../data/app.memrise.com.cookies.json";
-        private Dictionary<string, string> selectors = new Dictionary<string, string> {
-            { "back", "#__next > div > div > div.sc-36oahp-0.inakBl > div.sc-fbt2ce-0.blplTm > a:nth-child(2) > div" },
-            { "start", "#floatingBottomPortalRoot > div > div > div > div > div > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div.sc-1txhy2h-0.iVaTfJ > button > div" }
-        };
-        private ChromeDriver ?driver;
+        private const string CookiesPath = "../../../data/app.memrise.com.cookies.json", 
+            SelectorsPath = "../../../data/selectors.json";
+        private Dictionary<string, string> ?selectors;
+
+        private ChromeDriver ?driver = new ChromeDriver(); 
+        private Translator translator = new Translator();
 
         /// <summary>
         /// For ease of decoding the cookies json.
@@ -32,8 +34,11 @@ namespace MemriseBot.src {
         /// Initializes a new instance of the <see cref="Crawler"/> class, by opening a new window.
         /// </summary>
         public Crawler() { 
-            driver = new ChromeDriver(); 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            // timeout
+            driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            // selectors
+            selectors = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(SelectorsPath));
         }
 
         /// <summary>
@@ -60,12 +65,107 @@ namespace MemriseBot.src {
             AddCookies();
             driver!.Navigate().GoToUrl("https://app.memrise.com/dashboard/scenarios");
         }
+
+        /// <summary>
+        /// Learns a new word.
+        /// </summary>
+        private void Learn() {
+            string word = driver!.FindElement(By.CssSelector(selectors!["learn-word"])).Text,
+                translation = driver!.FindElement(By.CssSelector(selectors!["learn-translation"])).Text;
+
+            translator.Learn(word, translation);
+
+            // move on by pressing continue or next
+            try {
+                driver!.FindElement(By.CssSelector(selectors!["learn-continue"])).Click();
+                return;
+            }
+            catch (NoSuchElementException) {}
+
+            try { driver!.FindElement(By.CssSelector(selectors!["learn-next"])).Click(); }
+            catch (NoSuchElementException) {}
+        }
+
+        /// <summary>
+        /// Answers a multiple choice question.
+        /// </summary>
+        private void MultipleChoice() {
+            string word = driver!.FindElement(By.CssSelector(selectors!["multiple-choice-word"])).Text;
+            ReadOnlyCollection<IWebElement> optionDivs = driver!.FindElements(By.CssSelector(selectors!["multiple-choice-options"]));
+            
+            string ?translation = translator.Translate(word);
+            
+            // if the word is unknown, learn
+            if (translation == null) {
+                driver!.FindElement(By.CssSelector(selectors!["multiple-choice-I-don't-know"])).Click();
+                return;
+            }
+
+            // pick a choice
+            foreach (IWebElement option in optionDivs) {
+                if (option.Text != translation) continue;
+                option.Click();
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Answers a type question.
+        /// </summary>
+        private void TypeQuestion() {
+            string word = driver!.FindElement(By.CssSelector(selectors!["type-word"])).Text;
+            IWebElement input = driver!.FindElement(By.CssSelector(selectors!["typing-input"]));
+
+            string ?translation = translator.Translate(word);
+
+            // TODO
+
+        }
+
+        /// <summary>
+        /// Completes an exercise page, by first identifying what type of exercise it is.
+        /// </summary>
+        /// <remarks>Video/audio based exercises not supported.</remarks>
+        private void CompleteExercise() {
+            string prompt = null;
+            try { prompt = driver!.FindElement(By.CssSelector(selectors!["exercise-prompt"])).Text; }
+            catch (NoSuchElementException) {}
+
+            if (prompt == null) {
+                Learn();
+                return;
+            }
+
+            if (prompt == "Pick the correct translation") {
+                MultipleChoice();
+                return;
+            }
+
+            if (prompt == "Type the correct translation") {
+                TypeQuestion();
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
     
         /// <summary>
         /// Plays a game.
         /// </summary>
         public void Play() {
-            driver!.FindElement(By.CssSelector(selectors.)).Click();
+            // potentially close ad button
+            try { driver!.FindElement(By.CssSelector(selectors!["ad-back"])).Click(); }
+            catch (NoSuchElementException) {}
+
+            // start game
+            driver!.FindElement(By.CssSelector(selectors!["start"])).Click();
+
+            // potentially close ad button
+            try { driver!.FindElement(By.CssSelector(selectors["commit-ad-x"])).Click(); }
+            catch (NoSuchElementException) {}
+
+            // TODO
+            for (int i = 0; i < 10; i++) { CompleteExercise(); }
         }
     }
 }
