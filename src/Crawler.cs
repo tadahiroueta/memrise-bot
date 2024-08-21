9e9b72f1bd9919246c7a090441a4aad9fc2da7f3
tadahiroueta <tadahiroueta@gmail.com>
2024-08-20 21:50:33 -0500

    done

--- src/Crawler.cs
+++ src/Crawler.cs
@@ -13,11 +13,11 @@ namespace MemriseBot {
         private const string CookiesPath = "../../../data/app.memrise.com.cookies.json", 
             SelectorsPath = "../../../data/selectors.json",
             MemriseURL = "https://app.memrise.com/dashboard/scenarios";
-        private const int GeneralTimeout = 4;
+        private const int GeneralTimeout = 6;
         private const double QuestionTimeout = .5;
 
         private Dictionary<string, string> ?selectors;
-        private ChromeDriver ?driver = new ChromeDriver(); 
+        private ChromeDriver driver = new ChromeDriver(); 
         private Translator translator = new Translator();
 
         /// <summary>
@@ -38,7 +38,7 @@ namespace MemriseBot {
         /// </summary>
         public Crawler() { 
             // timeout
-            driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(GeneralTimeout);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(GeneralTimeout);
 
             // selectors
             selectors = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(SelectorsPath));
@@ -48,11 +48,11 @@ namespace MemriseBot {
         /// Logs in to the Memrise website with cookies.
         /// </summary>
         public void Login() {
-            driver!.Navigate().GoToUrl(MemriseURL);
+            driver.Navigate().GoToUrl(MemriseURL);
 
             List<Cookie> ?cookies = JsonConvert.DeserializeObject<List<Cookie>>(File.ReadAllText(CookiesPath));
             foreach (Cookie cookie in cookies!) { 
-                driver!.Manage().Cookies.AddCookie(new OpenQA.Selenium.Cookie(
+                driver.Manage().Cookies.AddCookie(new OpenQA.Selenium.Cookie(
                     cookie.name, 
                     cookie.value, 
                     cookie.domain, 
@@ -61,7 +61,7 @@ namespace MemriseBot {
                 ));
             }
 
-            driver!.Navigate().GoToUrl(MemriseURL);
+            driver.Navigate().GoToUrl(MemriseURL);
         }
 
         /// <summary>
@@ -72,20 +72,20 @@ namespace MemriseBot {
             string word = null, translation = null;
 
             try { 
-                word = driver!.FindElement(By.CssSelector(selectors!["learn-word"])).Text;
-                translation = driver!.FindElement(By.CssSelector(selectors!["learn-translation"])).Text; 
+                word = driver.FindElement(By.CssSelector(selectors!["learn-word"])).Text;
+                translation = driver.FindElement(By.CssSelector(selectors!["learn-translation"])).Text; 
             }
             catch (NoSuchElementException) {}
 
             try { 
-                word = driver!.FindElement(By.CssSelector(selectors!["learn-word-video"])).Text; 
-                translation = driver!.FindElement(By.CssSelector(selectors!["learn-translation-video"])).Text;
+                word = driver.FindElement(By.CssSelector(selectors!["learn-word-video"])).Text; 
+                translation = driver.FindElement(By.CssSelector(selectors!["learn-translation-video"])).Text;
             }
             catch (NoSuchElementException) {}
                     
             try { 
-                word = driver!.FindElement(By.CssSelector(selectors!["learn-word-correction"])).Text; 
-                translation = driver!.FindElement(By.CssSelector(selectors!["learn-translation-correction"])).Text;
+                word = driver.FindElement(By.CssSelector(selectors!["learn-word-correction"])).Text; 
+                translation = driver.FindElement(By.CssSelector(selectors!["learn-translation-correction"])).Text;
             }
             catch (NoSuchElementException) {}
 
@@ -99,12 +99,12 @@ namespace MemriseBot {
 
             // move on by pressing continue or next
             try {
-                driver!.FindElement(By.CssSelector(selectors!["learn-continue"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["learn-continue"])).Click();
                 return;
             }
             catch (NoSuchElementException) {}
 
-            try { driver!.FindElement(By.CssSelector(selectors!["learn-next"])).Click(); }
+            try { driver.FindElement(By.CssSelector(selectors!["learn-next"])).Click(); }
             catch (NoSuchElementException) {}
         }
 
@@ -112,13 +112,13 @@ namespace MemriseBot {
         /// Answers a multiple choice question.
         /// </summary>
         private void MultipleChoice() {
-            string word = driver!.FindElement(By.CssSelector(selectors!["exercise-word"])).Text;
-            ReadOnlyCollection<IWebElement> optionDivs = driver!.FindElements(By.CssSelector(selectors!["multiple-choice-options"]));
+            string word = driver.FindElement(By.CssSelector(selectors!["exercise-word"])).Text;
+            ReadOnlyCollection<IWebElement> optionDivs = driver.FindElements(By.CssSelector(selectors!["multiple-choice-options"]));
             string ?translation = translator.Translate(word);
             
             // if the word is unknown, learn
             if (translation == null) {
-                driver!.FindElement(By.CssSelector(selectors!["exercise-I-don't-know"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["exercise-I-don't-know"])).Click();
                 return;
             }
 
@@ -134,13 +134,13 @@ namespace MemriseBot {
         /// Answers a type question.
         /// </summary>
         private void TypeQuestion() {
-            string word = driver!.FindElement(By.CssSelector(selectors!["exercise-word"])).Text;
-            IWebElement input = driver!.FindElement(By.CssSelector(selectors!["typing-input"]));
+            string word = driver.FindElement(By.CssSelector(selectors!["exercise-word"])).Text;
+            IWebElement input = driver.FindElement(By.CssSelector(selectors!["typing-input"]));
             string ?translation = translator.Translate(word);
 
             // if the word is unknown, learn
             if (translation == null) {
-                driver!.FindElement(By.CssSelector(selectors!["exercise-I-don't-know"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["exercise-I-don't-know"])).Click();
                 return;
             }
 
@@ -158,7 +158,7 @@ namespace MemriseBot {
         /// <remarks>Video/audio based exercises not supported.</remarks>
         private void CompleteExercise() {
             string ?prompt = null;
-            try { prompt = driver!.FindElement(By.CssSelector(selectors!["exercise-prompt"])).Text; }
+            try { prompt = driver.FindElement(By.CssSelector(selectors!["exercise-prompt"])).Text; }
             catch (NoSuchElementException) {}
 
             if (prompt == null) {
@@ -179,52 +179,55 @@ namespace MemriseBot {
             throw new NotImplementedException();
         }
 
-                /// <summary>
+        /// <summary>
         /// Plays a session.
         /// </summary>
         /// <returns>The points earned in the session.</returns>
         private int PlaySession() {
             // potentially close ad button
-            try { driver!.FindElement(By.CssSelector(selectors!["commit-ad-x"])).Click(); }
+            try { driver.FindElement(By.CssSelector(selectors!["commit-ad-x"])).Click(); }
             catch (NoSuchElementException) {}
 
             // faster timeout for questions
-            driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(QuestionTimeout);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(QuestionTimeout);
 
             while (true) {
                 CompleteExercise();
 
                 // potentially close out of subscription page
-                try { driver!.FindElement(By.CssSelector(selectors!["subscription-maybe-later"])).Click(); }
+                try { driver.FindElement(By.CssSelector(selectors!["subscription-maybe-later"])).Click(); }
                 catch (NoSuchElementException) {}
 
                 // session complete
                 try {
-                    driver!.FindElement(By.CssSelector(selectors!["scenario-summary"]));
+                    driver.FindElement(By.CssSelector(selectors!["scenario-summary"]));
                     break;
                 }
                 catch (NoSuchElementException) {}
             }
 
             // slower
-            driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(GeneralTimeout);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(GeneralTimeout);
 
             // loading time
             Thread.Sleep(GeneralTimeout * 1000 * 3);
 
             // get points "earned"
-            string pointText = driver!.FindElement(By.CssSelector(selectors!["session-points"])).Text;
+            string pointText = driver.FindElement(By.CssSelector(selectors!["session-points"])).Text;
             int points = int.Parse(pointText.Split(" ")[0]);
 
             // press continue
-            driver!.FindElement(By.CssSelector(selectors!["scenario-summary"])).Click();
+            driver.FindElement(By.CssSelector(selectors!["scenario-summary"])).Click();
+
+            driver.FindElement(By.CssSelector(selectors!["summary-continue"])).Click();
+            Thread.Sleep(GeneralTimeout * 1000);
 
             // potentially close level up
-            try { driver!.FindElement(By.CssSelector(selectors!["level-up-continue"])).Click(); }
+            try { 
+                driver.FindElement(By.CssSelector(selectors!["level-up-continue"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["next-stage-continue"])).Click();
+            }
             catch (NoSuchElementException) {}
-
-            driver!.FindElement(By.CssSelector(selectors!["summary-continue"])).Click();
-            Thread.Sleep(GeneralTimeout * 1000);
 
             return points;
         }
@@ -233,19 +236,21 @@ namespace MemriseBot {
         /// Picks a "Learn words" session.
         /// </summary>
         private void PickSession() {
-            string title = driver!.FindElement(By.CssSelector(selectors!["session-title"])).Text;
+            string title = driver.FindElement(By.CssSelector(selectors!["session-title"])).Text;
             if (title == "Hear my words") {
-                driver!.FindElement(By.CssSelector(selectors!["left-arrow"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["left-arrow"])).Click();
             }
             if (title == "Use my words") {
-                driver!.FindElement(By.CssSelector(selectors!["right-arrow"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["right-arrow"])).Click();
             }
+
             Thread.Sleep(GeneralTimeout * 1000);
-            driver!.FindElement(By.CssSelector(selectors!["start"])).Click();
+            driver.FindElement(By.CssSelector(selectors!["start"])).Click();
+            Thread.Sleep(GeneralTimeout * 1000 * 2);
 
             // throwing error means the scenario is available
             try { 
-                driver!.FindElement(By.CssSelector(selectors!["skip-scenario"])).Click();
+                driver.FindElement(By.CssSelector(selectors!["skip-scenario"])).Click();
                 PickSession();
             }
             catch (NoSuchElementException) {}
@@ -259,7 +264,7 @@ namespace MemriseBot {
             int pointsLeft = goal;
 
             // potentially close pop ups
-            try { driver!.FindElement(By.CssSelector(selectors!["ad-back"])).Click(); }
+            try { driver.FindElement(By.CssSelector(selectors!["ad-back"])).Click(); }
             catch (NoSuchElementException) {}
  
             while (pointsLeft > 0) {
